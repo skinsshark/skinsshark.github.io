c5bf9f08ba754a0976deb0fe23a8f11e205cc34b
Sharon Zheng <sharzheng@gmail.com>
2022-09-26 21:36:59 -0700

    migrate to react

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {useEffect} from 'react';
+
+import Face from './Face';
+
+function calcValues() {
+  const centerContent = document.getElementById('center-content');
+  const centerFold = document.getElementById('center-fold');
+
+  const overflowHeight = centerContent.clientHeight - centerFold.clientHeight;
+
+  document.body.style.height = overflowHeight + window.innerHeight + 'px';
+  const foldsContent = Array.from(
+    document.getElementsByClassName('fold-content'),
+  );
+  const tick = () => {
+    const scroll = -(
+      document.documentElement.scrollTop || document.body.scrollTop
+    );
+    foldsContent.forEach(content => {
+      content.style.transform = `translateY(${scroll}px)`;
+    });
+    requestAnimationFrame(tick);
+  };
+
+  tick();
+}
+
+function App() {
+  useEffect(() => {
+    window.addEventListener('resize', calcValues);
+    calcValues();
+  }, []);
+
+  return (
+    <div className="all">
+      <div className="wrapper-3d">
+        <div className="fold fold-top">
+          <div className="fold-align">
+            <div className="fold-content">
+              <Face />
+            </div>
+          </div>
+        </div>
+        <div className="fold fold-center" id="center-fold">
+          <div className="fold-align">
+            <div className="fold-content" id="center-content">
+              <Face />
+            </div>
+          </div>
+        </div>
+        <div className="fold fold-bottom">
+          <div className="fold-align">
+            <div className="fold-content">
+              <Face />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Face.js
@@ -0,0 +1,301 @@
+import * as React from 'react';
+
+function Face() {
+  return (
+    <>
+      <header>
+        <h1>Sharon Zheng</h1>
+      </header>
+
+      <div className="container">
+        <article>
+          <ul>
+            <li>Front End Engineer — React Native @ Meta</li>
+            <li>sharzheng@gmail.com</li>
+            <li>
+              <a
+                href="https://github.com/skinsshark"
+                target="_blank"
+                rel="noopener noreferrer">
+                github.com/skinsshark
+              </a>
+            </li>
+          </ul>
+        </article>
+
+        <section>
+          <h2>Personal Projects</h2>
+          <hr />
+
+          <section>
+            <div>
+              <h3>
+                Curated Collection of Websites and Other Things &mdash; <br />
+                <i>Independent work I've created for fun</i>
+              </h3>
+            </div>
+            <div className="dates">
+              <h4>Jul 2017-now</h4>
+            </div>
+            <article>
+              <ul>
+                <li>[TBA] &mdash; An experimental mobile browser experience</li>
+                <li>
+                  [WIP]{' '}
+                  <a
+                    href="https://inner-self-portrait.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Inner Self Portrait
+                  </a>{' '}
+                  &mdash; Inspired by a creative workshop by{' '}
+                  <a
+                    href="https://www.instagram.com/facebookopenarts/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Open Arts
+                  </a>{' '}
+                  and{' '}
+                  <a
+                    href="https://www.stefanieposavec.com"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Stefanie Posavec
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://social-distance-this-for.me"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Social Distance This for Me
+                  </a>{' '}
+                  &mdash; Easy-to-add script to your website so we can get the
+                  internet to social distance too
+                </li>
+                <li>
+                  <a
+                    href="https://sharonzheng.com/kwaterloo"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Kitchener/Waterloo Guide
+                  </a>{' '}
+                  &mdash; A summary of my favorite places in the town I went to
+                  University
+                </li>
+                <li>
+                  <a
+                    href="https://sharonzheng.com/cny2020"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Chinese Zodiac
+                  </a>{' '}
+                  &mdash; Which Chinese zodiac animal are you?
+                </li>
+                <li>
+                  <a
+                    href="https://sharonzheng.com/london-2019"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Sharon London Tour (2019)
+                  </a>{' '}
+                  &mdash; London UK city guide
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/a/r/?effect_id=2969821319757901"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Fruit Sha La
+                  </a>{' '}
+                  &mdash; An Instagram face filter with over 75,000 impressions
+                </li>
+                <li>
+                  <a
+                    href="https://the-offisse.com/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    The Offisse
+                  </a>{' '}
+                  &mdash; High end fashion inspired by The Office (US)
+                </li>
+                <li>
+                  <a
+                    href="https://sharonzheng.com/tachi"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Tachi
+                  </a>{' '}
+                  &mdash; Photo gallery of a small omakase experience in Toronto
+                </li>
+                <li>
+                  <a
+                    href="https://sharonzheng.com/winyc"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    When in NYC
+                  </a>{' '}
+                  &mdash; A friend asked for recommendations, this is what they
+                  got
+                </li>
+                <li>
+                  <a
+                    href="https://medium.com/@sharonzheng"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    A How-To Guide for the Modern World
+                  </a>{' '}
+                  &mdash; Medium articles of various topics
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/skinsshark/waterflow"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Waterflow
+                  </a>{' '}
+                  &mdash; Chrome extension to make course selection research
+                  more efficient
+                </li>
+                <li>Many more as private repos...</li>
+              </ul>
+            </article>
+          </section>
+
+          <section>
+            <h3>Freelance Web Developer</h3>
+            <div className="dates">
+              <h4>Jun 2018-2020</h4>
+            </div>
+            <ul>
+              <li>
+                Highlights &mdash;{' '}
+                <a
+                  href="https://viv.co"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Vitals in View
+                </a>{' '}
+                &{' '}
+                <a
+                  href="https://esther-cheung.com/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  esther-cheung.com
+                </a>
+              </li>
+              <li>
+                Worked with clients to discuss design and develop a plan to meet
+                goals and objectives using Trello and Contentful CMS
+              </li>
+            </ul>
+          </section>
+
+          <section>
+            <h3>
+              Co-founder/Software Developer &mdash; <br />
+              <a
+                href="https://sundaydesert.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                sundaydesert.com
+              </a>
+            </h3>
+            <div className="dates">
+              <h4>Aug 2017-2018</h4>
+            </div>
+            <ul>
+              <li>
+                Founded studio to create interactive art using different
+                languages, frameworks and libraries
+              </li>
+              <li>
+                Collaborated with partner on design and worked independently on
+                development
+              </li>
+              <li>
+                Projects such as{' '}
+                <a
+                  href="https://iloveyouandyoulove.me"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  ILY&YLM
+                </a>
+              </li>
+            </ul>
+          </section>
+        </section>
+        <section>
+          <h2>Previous Work Experience</h2>
+          <hr />
+
+          <article>
+            <ul>
+              <li>
+                Facebook, London &mdash; <br />
+                <i>Front End Engineering Intern</i> (Sept-Dec 2019)
+              </li>
+              <li>
+                Facebook, New York &mdash; <br />{' '}
+                <i>Front End Engineering Intern</i> (Sept-Dec 2018)
+              </li>
+              <li>
+                Lyft, Seattle &mdash; <br />
+                <i>Software Engineer Intern</i> (Jan-Apr 2018)
+              </li>
+              <li>
+                Apple, Sunnyvale &mdash; <br />
+                <i>Software Engineer Intern</i> (May-Aug 2017)
+              </li>
+            </ul>
+          </article>
+        </section>
+
+        <section>
+          <h2>Education</h2>
+          <hr />
+
+          <section>
+            <div>
+              <h3>
+                Bachelor of Applied Science, Computer Engineering &mdash; <br />
+                University of Waterloo
+              </h3>
+            </div>
+            <div className="dates">
+              <h4>Apr 2020</h4>
+            </div>
+          </section>
+        </section>
+
+        <section>
+          <h2>Things I Like</h2>
+          <hr />
+
+          <section>
+            <div>
+              <p>
+                Visiting modern art museums, Olafur Eliasson's "Din blinde
+                passage (Your blind passenger)", cross stitching, boxing, hot
+                yoga, trying to guess your zodiac sign, thinking of joining a
+                gym, furniture shopping, white tea, and black coffee
+              </p>
+            </div>
+          </section>
+        </section>
+
+        <footer>
+          <a
+            href="./files/Resume-Sharon_Zheng.pdf"
+            target="_blank"
+            rel="noopener noreferrer">
+            PDF Version (last updated: Aug 2020)
+          </a>
+          <p>Happy to chat, reach out!</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default Face;
