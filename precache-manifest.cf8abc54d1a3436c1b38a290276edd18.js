c1a9d196915638c1e7b275ec91e930f897b37d0d
skinsshark <sharzheng@gmail.com>
2023-04-03 17:21:08 -0700

    Updates

---
+++ precache-manifest.cf8abc54d1a3436c1b38a290276edd18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe4b031040ba9c40710269ffd60a289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb8dbc9d96e0ef09838",
+    "url": "/static/css/main.13dbc4b0.chunk.css"
+  },
+  {
+    "revision": "52747c58bcf60b2133fc",
+    "url": "/static/js/2.b7430e8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7430e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb8dbc9d96e0ef09838",
+    "url": "/static/js/main.be508cb0.chunk.js"
+  },
+  {
+    "revision": "561fea4cbf860cd56c2a",
+    "url": "/static/js/runtime-main.30b9f472.js"
+  }
+]);
